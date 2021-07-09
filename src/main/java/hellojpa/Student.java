036bdc4402cee1995e5bc48268978d7855fd6a21
JinYeongKim <handayoung11@gmail.com>
2021-07-09 23:56:33 +0900

    생성자 추가

--- src/main/java/hellojpa/Student.java
+++ src/main/java/hellojpa/Student.java
@@ -15,4 +15,11 @@ public class Student {
     private Long id;
     private int age;
     private String name;
+
+    public Student() { }
+    public Student(Long id, int age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
 }
