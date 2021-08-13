eb110c09873e2599d38f2fa8efd8ecc66be1337b
JinYeongKim <handayoung11@gmail.com>
2021-08-14 08:59:37 +0900

    Student와 Subject를 매핑테이블인 Studentsubject를 통해 매핑하는 코드 작성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class JpaMain {
@@ -16,23 +17,42 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Subject math = new Subject();
+            math.setName("수학");
+            em.persist(math);
+
+            Subject history = new Subject();
+            history.setName("한국사");
+            em.persist(history);
+
             Student codeMania = new Student();
             codeMania.setName("code-mania");
             codeMania.setAge(21);
-//            codeMania.setClub(club);
+//            codeMania.getSubjects().add(math);
+//            codeMania.getSubjects().add(history);
             em.persist(codeMania);
 
             Student codeLover = new Student();
             codeLover.setName("code-lover");
             codeLover.setAge(21);
-//            codeLover.setClub(club);
+//            codeLover.getSubjects().add(history);
             em.persist(codeLover);
 
-            Club club = new Club();
-            club.setName("프로그래밍부");
-            club.getStudents().add(codeMania);
-            club.getStudents().add(codeLover);
-            em.persist(club);
+            StudentSubject studentSubject = new StudentSubject();
+            studentSubject.setStudent(codeMania);
+            studentSubject.setSubject(math);
+            em.persist(studentSubject);
+
+            StudentSubject studentSubject2 = new StudentSubject();
+            studentSubject2.setStudent(codeMania);
+            studentSubject2.setSubject(history);
+            em.persist(studentSubject2);
+
+            StudentSubject studentSubject3 = new StudentSubject();
+            studentSubject3.setStudent(codeLover);
+            studentSubject3.setSubject(history);
+            em.persist(studentSubject3);
+
             tx.commit();
         } catch(Exception e) {
             e.printStackTrace();
