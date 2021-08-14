34df3ad877174895a57af939765dad822d24d41e
JinYeongKim <handayoung11@gmail.com>
2021-08-14 13:43:07 +0900

    주석 수정

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,8 +4,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.time.LocalDateTime;
-import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -17,37 +15,41 @@ public class JpaMain {
         tx.begin();
 
         try {
+            // 수학 과목 등록
             Subject math = new Subject();
             math.setName("수학");
             em.persist(math);
 
+            // 한국사 과목 등록
             Subject history = new Subject();
             history.setName("한국사");
             em.persist(history);
 
+            // codeMania 학생 등록
             Student codeMania = new Student();
             codeMania.setName("code-mania");
             codeMania.setAge(21);
-//            codeMania.getSubjects().add(math);
-//            codeMania.getSubjects().add(history);
             em.persist(codeMania);
 
+            // codeLover 학생 등록
             Student codeLover = new Student();
             codeLover.setName("code-lover");
             codeLover.setAge(21);
-//            codeLover.getSubjects().add(history);
             em.persist(codeLover);
 
+            // codeMania 학생 수학 과목 수강
             StudentSubject studentSubject = new StudentSubject();
             studentSubject.setStudent(codeMania);
             studentSubject.setSubject(math);
             em.persist(studentSubject);
 
+            // codeMania 학생 한국사 과목 수강
             StudentSubject studentSubject2 = new StudentSubject();
             studentSubject2.setStudent(codeMania);
             studentSubject2.setSubject(history);
             em.persist(studentSubject2);
 
+            // codeLover 학생 한국사 과목 수강
             StudentSubject studentSubject3 = new StudentSubject();
             studentSubject3.setStudent(codeLover);
             studentSubject3.setSubject(history);
