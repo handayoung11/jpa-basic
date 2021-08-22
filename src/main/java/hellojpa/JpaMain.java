14b2369887ee0eeef15e52d354eac6ba33b0099a
JinYeongKim <handayoung11@gmail.com>
2021-08-23 01:50:15 +0900

    proxy 타입체크, 동일성보장 테스트, 관리, 에러체크 코드 작성

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,63 +1,123 @@
-    package hellojpa;
+package hellojpa;
 
-    import javax.persistence.EntityManager;
-    import javax.persistence.EntityManagerFactory;
-    import javax.persistence.EntityTransaction;
-    import javax.persistence.Persistence;
+import org.hibernate.Hibernate;
+import org.hibernate.LazyInitializationException;
 
-    public class JpaMain {
-        public static void main(String[] args) {
-            EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 
-            EntityManager em = emf.createEntityManager();
-            EntityTransaction tx = em.getTransaction();
+public class JpaMain {
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+    static EntityManager em = emf.createEntityManager();
 
-            tx.begin();
+    public static void main(String[] args) {
+        EntityTransaction tx = em.getTransaction();
 
-            try {
-                // programming club 생성
-                Club club = new Club();
-                club.setName("programming");
-                club.setDescription("즐거운 프로그래밍?");
-                club.setCreator("code-mania");
-                em.persist(club);
+        tx.begin();
 
-                // code-mania student 생성
-                Student student = new Student();
-                student.setName("code-mania");
-                student.setClub(club);
-                student.setCreator("code-mania");
-                em.persist(student);
+        try {
+            // programming club 생성
+            Club club = new Club();
+            club.setName("programming");
+            club.setDescription("즐거운 프로그래밍?");
+            club.setCreator("code-mania");
+            em.persist(club);
 
-                em.flush();
-                em.clear();
+            // code-mania student 생성
+            Student student = new Student();
+            student.setName("code-mania");
+            student.setClub(club);
+            student.setCreator("code-mania");
+            em.persist(student);
 
-                System.out.println("0. ======================="); // 0번 checkpoint
-                Student refMember = em.getReference(Student.class, student.getId());
-                System.out.println("1. refMember.getClass() = " + refMember.getClass()); // 1번 print
-                System.out.println("2. refMember.getId() = " + refMember.getId()); // 2번 print
-                System.out.println("3. refMember.getName() = " + refMember.getName()); // 3번 print
-                System.out.println("=======================");
+            clearEm();
 
-                em.flush();
-                em.clear();
+            // 타입 체크 코드
+            Student refStudent = em.getReference(Student.class, student.getId());
+            isStudent(refStudent);
 
-                System.out.println("4. ======================="); // 4번 checkpoint
-                Student findMember = em.find(Student.class, student.getId());
-                System.out.println("5. findMember.getClass() = " + findMember.getClass()); // 5번 print
-                System.out.println("6. findMember.getId() = " + findMember.getId()); // 6번 print
-                System.out.println("7. findMember.getName() = " + findMember.getName()); // 7번 print
-                System.out.println("=======================");
+            clearEm();
 
-                tx.commit();
-            } catch (Exception e) {
-                tx.rollback();
-                e.printStackTrace();
-            } finally {
-                em.close();
-            }
+            // 동일성 보장 테스트 코드
+            checkIdentity(student);
 
+            clearEm();
+
+            // 프록시 관리
+            proxyUtill(em, student);
+
+            clearEm();
+
+            // 에러 체크 코드
+            errorCheck(student);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
             em.close();
-            emf.close();
+        }
+
+        em.close();
+        emf.close();
+    }
+
+    public static void clearEm() {
+        em.flush();
+        em.clear();
+    }
+
+    public static void checkIdentity(Student student) {
+        Student refStudent = em.getReference(Student.class, student.getId());
+        Student findStudent = em.find(Student.class, student.getId());
+
+        System.out.println("refStudent = " + refStudent.getClass());
+        System.out.println("findStudent = " + findStudent.getClass());
+        System.out.println("(refStudent == findStudent) = " + (refStudent == findStudent));
+
+        clearEm();
+
+        Student findStudent2 = em.find(Student.class, student.getId());
+        Student refStudent2 = em.getReference(Student.class, student.getId());
+
+        System.out.println("refStudent2 = " + refStudent2.getClass());
+        System.out.println("findStudent2 = " + findStudent2.getClass());
+        System.out.println("(refStudent2 == findStudent2) = " + (refStudent2 == findStudent2));
+    }
+
+    public static boolean isStudent(Object student) {
+        // ==으로 타입 체크
+        System.out.println("student = " + (student.getClass() == Student.class));
+        // instanceof로 타입체크
+        System.out.println("student instanceof Student = " + (student instanceof Student));
+        return student instanceof Student;
+    }
+
+    public static void errorCheck(Student student) {
+        try {
+            Student refStudent = em.getReference(Student.class, student.getId());
+            // refStudent 준영속화
+            em.detach(refStudent);
+            // refStudent 초기화
+            refStudent.getName();
+        } catch (LazyInitializationException e) {
+            System.out.println("0. error====================================");
+            e.printStackTrace();
         }
     }
+
+    public static void proxyUtill(EntityManager em, Student student) {
+        System.out.println("1. proxy utill====================================");
+        Student refStudent = em.getReference(Student.class, student.getId());
+        // proxy class 이름 확인
+        System.out.println("refStudent.getClass().getName() = " + refStudent.getClass().getName());
+        // proxy class 초기화 여부 확인
+        System.out.println("emf.getPersistenceUnitUtil().isLoaded(refStudent) = " + emf.getPersistenceUnitUtil().isLoaded(refStudent));
+        // proxy class 초기화
+        Hibernate.initialize(refStudent);
+        System.out.println("emf.getPersistenceUnitUtil().isLoaded(refStudent) = " + emf.getPersistenceUnitUtil().isLoaded(refStudent));
+    }
+}
