9aee0bb5e22af2cfb54a34a61b9ddd57aa21fb15
JinYeongKim <handayoung11@gmail.com>
2021-09-09 23:37:50 +0900

    JPQL과 JPA Criteria 사용해보기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,8 +4,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
-import java.util.Set;
 
 public class JpaMain {
     static EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
@@ -17,42 +19,22 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 저장 코드
-            Student student = new Student();
-            student.setName("code-mania");
+            List<Student> resultList = em.createQuery("select s from Student s where s.name like '%김%'", Student.class)
+                    .getResultList();
 
-            student.getFavoriteSubjects().add("수학");
-            student.getFavoriteSubjects().add("영어");
-            student.getFavoriteSubjects().add("역사");
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Student> query = cb.createQuery(Student.class);
+            Root<Student> s = query.from(Student.class);
 
-            student.getAddressHistory().add(new AddressEntity("서울특별시", "세종대로", "10000"));
-            student.getAddressHistory().add(new AddressEntity("인천광역시", "부평대로", "60000"));
+            CriteriaQuery<Student> cq = query.select(s);
 
-            em.persist(student);
+            //동적쿼리
+            String name = "kim";
+            if(name != null) cq.where(cb.like(s.get("name"), name));
 
-            em.flush();
-            em.clear();
+            List<Student> students = em.createQuery(cq).getResultList();
 
-            // 조회 코드
-            System.out.println("============================");
-            Student findStudent = em.find(Student.class, student.getId());
-            List<AddressEntity> addressHistory = findStudent.getAddressHistory();
-            for (AddressEntity address : addressHistory) {
-                System.out.println("address.getCity() = " + address.getAddress().getCity());
-            }
-
-            Set<String> favoriteSubjects = findStudent.getFavoriteSubjects();
-            for (String favoriteSubject : favoriteSubjects) {
-                System.out.println("favoriteSubject = " + favoriteSubject);
-            }
-            System.out.println("============================");
-
-            // 수정 코드
-            favoriteSubjects.remove("역사");
-            favoriteSubjects.add("과학");
-
-            Address address = new Address("서울특별시", "세종대로", "10000");
-            findStudent.getAddressHistory().get(0).setAddress(new Address(address.getCity(), address.getStreet(), "65202"));
+            List students2 = em.createNativeQuery("select * from student", Student.class).getResultList();
 
             tx.commit();
         } catch (Exception e) {
